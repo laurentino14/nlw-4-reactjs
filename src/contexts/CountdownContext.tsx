06fb144dfdf2ea654edb312ebc0b82347056facd
laurentino14 <laurentino14@outlook.com>
2022-05-21 23:03:20 -0300

    1.0

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -18,7 +18,7 @@ export const CountdownContext = createContext({} as CountdownContextData);
 let countdownTimeout: NodeJS.Timeout;
 
 export function CountdownProvider({children}: CountdownProviderProps) {
-  const [time, setTime] = useState(0.1 * 60);
+  const [time, setTime] = useState(25 * 60);
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
   const minutes = Math.floor(time / 60);
@@ -32,7 +32,7 @@ export function CountdownProvider({children}: CountdownProviderProps) {
   function resetCountdown() {
     clearTimeout(countdownTimeout);
     setIsActive(false);
-    setTime(0.1 * 60);
+    setTime(25 * 60);
     setHasFinished(false);
   }
 
